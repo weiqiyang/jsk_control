5b1e108d02f8cfaa52033d1badd1d4f653b4ffc4
Iki Yo <yo@jsk.imi.i.u-tokyo.ac.jp>
2019-11-18 19:16:54 +0900

    Add hsrb_simple_go_spot plugin and sample.

--- jsk_teleop_joy/src/jsk_teleop_joy/plugin/hsrb_simple_go_spot.py
+++ jsk_teleop_joy/src/jsk_teleop_joy/plugin/hsrb_simple_go_spot.py
@@ -15,31 +15,31 @@ except:
 
 from sensor_msgs.msg import Joy
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseStamped
 from diagnostic_msgs.msg import DiagnosticStatus, DiagnosticArray
 import tf.transformations
 
+from jsk_hsr_startup import SpotMixin
 from jsk_teleop_joy.joy_plugin import JSKJoyPlugin
 from jsk_rviz_plugins.msg import OverlayMenu
 from status_history import StatusHistory
 
-class DynamicMenu(JSKJoyPlugin):
+class HsrbSimpleGoSpot(JSKJoyPlugin):
   '''
 Usage:
+This plugin calls ROS services for HSR to get saved spots and send go_to_spot order.
+
 Up/Down: choose from menu
 
 circle/square/triangle: publish cooperating command
 
 Args:
-frame_id [String, default: map]: frame_id of publishing pose
-title [String, default: PoseList]: title of the menu
-history [String, default: history]: rosparam name to load pose list
-set_pose [String, default: set_pose]: topic name for publishing pose topic to set master
+prefix [String, default: ]: prefix of the spots to be listed; it will be obmitted from the spots' name
+title [String, default: prefix/SpotList]: title of the menu; it is the same as prefix by default; if prefix is an empty string, default value is 'SpotList'
 topic [String, default: dynamic_menu]: topic name to publish the menu
 command [String, default: command]: topic name for publishing the command
-triangle_cmd [String, default: DM_TRIANGLE_CMD]: command text when triangle button is pressed
-circle_cmd [String, default: DM_CIRCLE_CMD]: command text when triangle button is pressed
-square_cmd [String, default: DM_SQUARE_CMD]: command text when triangle button is pressed
+triangle_cmd [String, default: GS_TRIANGLE_CMD]: command text when triangle button is pressed
+circle_cmd [String, default: GS_CIRCLE_CMD]: command text when triangle button is pressed
+square_cmd [String, default: GS_SQUARE_CMD]: command text when triangle button is pressed
   '''
   STATE_INITIALIZATION = 1
   STATE_RUNNING = 2
@@ -65,31 +65,31 @@ square_cmd [String, default: DM_SQUARE_CMD]: command text when triangle button i
 
   def __init__(self, name, args):
     JSKJoyPlugin.__init__(self, name, args)
+    self.spot_mixin = SpotMixin()
     self.current_item_index = 0
     self.selecting_item_index = 0
-    self.title = self.getArg('title', 'PoseList')
-    self.list_name = self.getArg('history', 'history')
-    self.pose_pub = rospy.Publisher(self.getArg('set_pose', 'set_pose'),
-                                      PoseStamped, queue_size=10)
+    self.prefix = self.getArg('prefix','')
+    self.title = self.getArg('title', self.prefix)
+    if self.title == '':
+      self.title = 'SpotList'
     self.menu_pub = rospy.Publisher(self.getArg('topic', 'dynamic_menu'),
                                       OverlayMenu, queue_size=10)
-    self.frame_id = self.getArg('frame_id', 'map')
-
     self.command_pub = rospy.Publisher(self.getArg('command', 'command'),
                                     String, queue_size=1)
-    self.triangle_cmd = self.getArg('triangle_cmd', 'DM_TRIANGLE_CMD')
-    self.square_cmd = self.getArg('square_cmd', 'DM_SQUARE_CMD')
-    self.circle_cmd = self.getArg('circle_cmd', 'DM_CIRCLE_CMD')
+    self.triangle_cmd = self.getArg('triangle_cmd', 'GS_TRIANGLE_CMD')
+    self.square_cmd = self.getArg('square_cmd', 'GS_SQUARE_CMD')
+    self.circle_cmd = self.getArg('circle_cmd', 'GS_CIRCLE_CMD')
 
     self.loadItems()
     self.start()
 
   def loadItems(self):
-    if rospy.has_param(self.list_name):
-      self.item_instances = rospy.get_param(self.list_name)
-    else:
-      rospy.set_param(self.list_name, [])
-      self.item_instances = []
+    self.item_instances = []
+    spots = self.spot_mixin.get_spots()
+    start = len(self.prefix)
+    for s in spots:
+      if s.startswith(self.prefix):
+        self.item_instances.append(s[start:])
 
   def switchItem(self, index):
     self.current_item_index = index
@@ -103,16 +103,16 @@ square_cmd [String, default: DM_SQUARE_CMD]: command text when triangle button i
   def start(self):
     self.publishMenu(0, close=True) # close menu anyway
     if len(self.item_instances) == 0:
-      rospy.logfatal('no valid history items are loaded')
+      rospy.logfatal('no valid spots are loaded')
       return False
     self.current_item = self.item_instances[0]
     return True
 
   def publishMenu(self, index, close=False):
     menu = OverlayMenu()
-    menu.menus = [i[0] for i in self.item_instances]
+    menu.menus = self.item_instances
     menu.current_index = index
-    menu.title = "PoseList"
+    menu.title = self.title
     if close:
       menu.action = OverlayMenu.ACTION_CLOSE
     self.menu_pub.publish(menu)
@@ -135,6 +135,8 @@ square_cmd [String, default: DM_SQUARE_CMD]: command text when triangle button i
         self.switchItem(self.selecting_item_index)
       elif status.circle and not latest.circle:
         #TODO action circle
+        # rospy.loginfo("go_to_spot: " + self.prefix + self.current_item)
+        self.spot_mixin.go_to_spot(self.prefix + self.current_item)
         self.command_pub.publish(self.circle_cmd)
         rospy.loginfo("circle")
       elif status.triangle and not latest.triangle:
@@ -151,21 +153,9 @@ square_cmd [String, default: DM_SQUARE_CMD]: command text when triangle button i
         self.publishMenu(self.selecting_item_index)
 
   def display(self, item):
-    rospy.loginfo("Load pose: " + str(item))
-    pose = PoseStamped()
-    pose.header.frame_id = self.frame_id
-    pose.header.stamp = rospy.Time(0.0)
-    pose.pose.position.x = item[1]
-    pose.pose.position.y = item[2]
-    pose.pose.position.z = item[3]
-    pose.pose.orientation.x = item[4]
-    pose.pose.orientation.y = item[5]
-    pose.pose.orientation.z = item[6]
-    pose.pose.orientation.w = item[7]
-    self.pose_pub.publish(pose)
+    rospy.loginfo("Selecting spot: " + self.prefix + item)
 
   def enable(self):
-    self.loadItems()
     self.start()
     # TODO save the previous pose?
     self.selecting_item_index = 0
